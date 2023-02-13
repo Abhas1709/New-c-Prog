78e0083ac677d11ce275f1fe0c3b4b406f1b42d4
Abhas Mishra <abhasmishra9336@gmail.com>
2023-02-13 23:48:43 +0530

    population

---
+++ population/POPULATION.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()  
+{
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c,d;
+	    cin>>a>>b>>c;
+	   d=a-b+c ;
+    cout<<d<<endl;	   
+	 
+	}
+	return 0;
+}
