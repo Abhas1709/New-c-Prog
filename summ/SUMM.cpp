36b26d83aeb6d5a9d2af288580959165fe15702d
Abhas Mishra <abhasmishra9336@gmail.com>
2023-02-14 23:53:34 +0530

    SUMM

---
+++ summ/SUMM.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	int a,b,c;
+	cin>>a;
+	cin>>b>>c;
+	if(a+b == c){
+	cout<<"yes"<<endl;
+	}
+	else
+	    cout<<"NO"<<endl;
+	
+	}
+	return 0;
+}
