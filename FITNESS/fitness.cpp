663c55658afddbd4e158720dbf1d2e517a389b72
Abhas Mishra <abhasmishra9336@gmail.com>
2023-02-16 23:54:18 +0530

    fitness

---
+++ FITNESS/fitness.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a;
+	    cin>>a;
+	    int b;
+	    b=2*a;
+	    cout<<5*b<<endl;
+	    
+	}
+	return 0;
+}
